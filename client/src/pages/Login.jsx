68d07142b12c95de820d64154ccaeb52e5cb1d9d
Vinayak Shree <vinayakshree2004@gmail.com>
2025-01-16 13:59:49 +0530

    Updated Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -48,11 +48,11 @@ const Login = () => {
         <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
           <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
             <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">
-              Manage all your task in one place!
+              Manage all College Events in one place!
             </span>
-            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-orange-600">
               <span>Cloud-Based</span>
-              <span>Task Manager</span>
+              <span>Event Manager</span>
             </p>
 
             <div className="cell">
@@ -68,7 +68,7 @@ const Login = () => {
             className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
           >
             <div className="">
-              <p className="text-blue-600 text-3xl font-bold text-center">
+              <p className="text-orange-500 text-3xl font-bold text-center">
                 Welcome back!
               </p>
               <p className="text-center text-base text-gray-700 ">
@@ -108,7 +108,7 @@ const Login = () => {
             {isLoading? <Loading/> :<Button
               type="submit"
               label="Submit"
-              className="w-full h-10 bg-blue-700 text-white rounded-full"
+              className="w-full h-10 bg-orange-600 text-white rounded-full"
             />}
             </div>
           </form>
